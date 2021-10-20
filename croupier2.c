bffdff1d08b71de8dc3ba121f6099a33ec0ba707
Dartagnaa <73881529+Dartagnaa@users.noreply.github.com>
2021-10-20 11:28:07 -0700

    Add files via upload

---
+++ croupier2.c
@@ -0,0 +1,233 @@
+/*----------------------------------------------
+Serveur à lancer avant le client
+------------------------------------------------*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <linux/types.h> 	/* pour les sockets */
+#include <sys/socket.h>
+#include <netdb.h> 		/* pour hostent, servent */
+#include <string.h> 		/* pour bcopy, ... */  
+#define TAILLE_MAX_NOM 256
+
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in sockaddr_in;
+typedef struct hostent hostent;
+typedef struct servent servent;
+
+/* Tableau dynamique de socket des joueurs */
+typedef struct joueur
+{
+	struct sockaddr socket;
+	struct joueur * suiv;
+}JOUEUR;
+
+/*----------------------------------------*/
+
+/*      FONCTIONS POUR LE BLACK JACK     */
+
+/*---------------------------------------*/
+/* Choisir une carte */
+int choix_carte(int cards[])
+{
+	int t;
+	int i;
+	int desk[52];	
+	for (i=0;i<52;i++){
+		desk[i] = (i/13+3)*100 + i%13 + 1;
+    }
+	srand(time(NULL));
+	for (i = 0; i < 52; i++)
+	{
+		do
+		{
+			t = rand() % 52;
+		} while (desk[t] == 0);
+		cards[i] = desk[t];
+		desk[t] = 0;
+	}
+	
+	return 0;
+}
+
+/* jouer une manche */
+int jouer(int sock)
+{
+  	int i;
+	int psum=0;
+	int bsum=0;
+	int pcards[5]={0};
+	int bcards[5]={0};
+	int cards[52];
+	int nbJoueurs;
+	char d;
+ 	char buffer[256];
+
+	sprintf(buffer, "joueur");
+  	//renvoi(sock, , ,buffer);
+	int longueur;
+  	while((longueur = read(sock, buffer, sizeof(buffer))) > 0) {
+    //sprintf(nbJoueurs,int atoi(buffer));
+  }
+	printf("\n");
+}
+
+
+
+/*------------------------------------------------------*/
+
+/*             FONCTIONS PARTIE RESEAUX                 */
+  
+/*------------------------------------------------------*/
+void reinitialise_buffer(char buffer[]){
+    for (int i = 0; i < 256; i++)
+    {
+        buffer[i] = ' ';
+    }
+}
+
+int nbrJoueur(int sock, char buffer[]){
+    int longueur;
+   
+    /* on lit ce qu'on a reçu du client */
+    if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) 
+    	return;
+
+    char reponse[256];
+
+    for (int i = 0; i < longueur; i++)
+    {
+        reponse[i]=buffer[i];
+    }
+
+    
+}
+
+/* recuperer la reponse du joueur */
+char * reponse_joueur(int sock, char buffer[]) {
+
+    int longueur;
+   
+   /* on lit ce qu'on a reçu du client */
+    if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) 
+    	return;
+
+    char reponse[256];
+
+    for (int i = 0; i < longueur; i++)
+    {
+        reponse[i]=buffer[i];
+    }
+
+    return reponse;
+}
+
+/* Envoyer un message au client */
+void envoi(int sock, char buffer[], char* message) {
+
+    int longueur;
+   
+   /* on lit ce qu'on a reçu du client */
+    if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) 
+    	return;
+
+    /* modifie le buffer pour le renvoyer */
+    reinitialise_buffer(buffer);    /*buffer vide*/
+
+    /* copier le message dans le buffer */
+    sprintf(buffer,message);
+    
+    /* mise en attente du programme pour simuler un delai de transmission */
+    sleep(3);
+    
+	/*ssize_t write(int fs, const void *buf, size_t N);*/
+    /* envoi du message au client*/
+    write(sock,buffer,strlen(buffer)+1);
+
+    return;
+    
+}
+
+
+main(int argc, char **argv) {
+  
+    int 		socket_descriptor, 		/* descripteur de socket */
+			nouv_socket_descriptor, 	/* [nouveau] descripteur de socket */
+			longueur_adresse_courante; 	/* longueur d'adresse courante d'un client */
+    sockaddr_in 	adresse_locale, 		/* structure d'adresse locale*/
+			adresse_client_courant; 	/* adresse client courant */
+    hostent*		ptr_hote; 			/* les infos recuperees sur la machine hote */
+    servent*		ptr_service; 			/* les infos recuperees sur le service de la machine */
+    char 		machine[TAILLE_MAX_NOM+1]; 	/* nom de la machine locale */
+    char        buffer[256];
+    char*       msg;                   /* message à renvoyer au client */
+
+    gethostname(machine,TAILLE_MAX_NOM);		/* recuperation du nom de la machine */
+    
+    /* recuperation de la structure d'adresse en utilisant le nom */
+    if ((ptr_hote = gethostbyname(machine)) == NULL) {
+		perror("erreur : impossible de trouver le serveur a partir de son nom.");
+		exit(1);
+    }
+    
+    /* initialisation de la structure adresse_locale avec les infos recuperees */			
+    
+    /* copie de ptr_hote vers adresse_locale */
+    bcopy((char*)ptr_hote->h_addr, (char*)&adresse_locale.sin_addr, ptr_hote->h_length);
+    adresse_locale.sin_family		= ptr_hote->h_addrtype; 	/* ou AF_INET */
+    adresse_locale.sin_addr.s_addr	= INADDR_ANY; 			/* ou AF_INET */
+    
+    /*-----------------------------------------------------------*/
+
+    /* Port : utiliser un service existant, par ex. "irc" */
+    if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
+		perror("erreur : impossible de recuperer le numero de port du service desire.");
+		exit(1);
+    }
+    adresse_locale.sin_port = htons(ptr_service->s_port);
+
+    /*-----------------------------------------------------------*/
+    
+    printf("numero de port pour la connexion au serveur : %d \n", 
+		   ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/);
+    
+    /* creation de la socket */
+    if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("erreur : impossible de creer la socket de connexion avec le client.");
+		exit(1);
+    }
+
+    /* association du socket socket_descriptor à la structure d'adresse adresse_locale */
+    if ((bind(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
+		perror("erreur : impossible de lier la socket a l'adresse de connexion.");
+		exit(1);
+    }
+    
+    /* initialisation de la file d'ecoute */
+    listen(socket_descriptor,5);
+
+    /* attente des connexions et traitement des donnees recues */
+    for(;;) {
+    
+		longueur_adresse_courante = sizeof(adresse_client_courant);
+		
+		/* adresse_client_courant sera renseigné par accept via les infos du connect */
+		if ((nouv_socket_descriptor = 
+			accept(socket_descriptor, 
+			       (sockaddr*)(&adresse_client_courant),
+			       &longueur_adresse_courante))
+			 < 0) {
+			perror("erreur : impossible d'accepter la connexion avec le client.");
+			exit(1);
+		}
+		
+		/* traitement du message */
+		printf("reception d'un message.\n");
+		
+		envoi(nouv_socket_descriptor,buffer,msg);
+						
+		close(nouv_socket_descriptor);
+		
+    }
+    
+}
---
+++ joueur2.c
@@ -0,0 +1,226 @@
+/*-----------------------------------------------------------
+Client a lancer apres le serveur avec la commande :
+client <adresse-serveur> <message-a-transmettre>
+------------------------------------------------------------*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <linux/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+typedef struct sockaddr 	sockaddr;
+typedef struct sockaddr_in 	sockaddr_in;
+typedef struct hostent 		hostent;
+typedef struct servent 		servent;
+
+/*-------------------------------------------------------*/
+
+void affichage_carte(int num)
+{
+	char fl;
+	int po_num;
+	
+	fl = num / 100;
+	po_num = num % 100;
+	switch (po_num)
+	{
+		case 1: 
+		{
+			printf("*******\n");
+			printf("*     *\n");
+			printf("* %c   *\n", fl);
+			printf("*   A *\n");
+			printf("*     *\n");
+			printf("*******\n");
+			break;
+		}
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		{
+			printf("*******\n");
+			printf("*     *\n");
+			printf("* %c   *\n", fl);
+			printf("*  %2d *\n", po_num);
+			printf("*     *\n");
+			printf("*******\n");
+			break;
+		}
+		case 11:
+		{
+			printf("*******\n");
+			printf("*     *\n");
+			printf("* %c   *\n", fl);
+			printf("*   v *\n");
+			printf("*     *\n");
+			printf("*******\n");
+			break;
+		}
+		case 12:
+		{
+			printf("*******\n");
+			printf("*     *\n");
+			printf("* %c   *\n", fl);
+			printf("*   D *\n");
+			printf("*     *\n");
+			printf("*******\n");
+			break;
+		}
+		case 13:
+		{
+			printf("*******\n");
+			printf("*     *\n");
+			printf("* %c   *\n", fl);
+			printf("*   R *\n");
+			printf("*     *\n");
+			printf("*******\n");
+			break;
+		}
+
+	}
+}
+/*------------------------------------------------------*/
+
+/*------------------------------------------------------*/
+void renvoi (int sock,char buffer[]) {
+
+    int longueur;
+   
+    if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) 
+    	return;
+    
+
+    buffer[longueur] = '#';
+    buffer[longueur+1] ='\0';
+    
+
+    /* mise en attente du prgramme pour simuler un delai de transmission */
+    sleep(3);
+    
+	/*ssize_t write(int fs, const void *buf, size_t N);*/
+    write(sock,buffer,strlen(buffer)+1);
+    
+    printf("message envoye. \n");
+
+    buffer={0};
+
+    return;
+}
+
+/*------------------------------------------------------*/
+void affichage_regle(){
+	printf("Le but est de faire 21 points.");
+	printf("L'As vaut 1 ou 11, et les têtes valent 10");
+	printf("Si vous faites 21 points sue les 2 première cartes : Black Jack. Vous gagnez la manche");
+	printf("Sinon, vous pouvez piocher une carte. Mais attention, il ne faut pas dépasser 21");
+	printf("Si vous dépassez 21, vous perdez la manche, si vous faites moins, c'est au tour du croupier de tirer une carte");
+	printf("Le croupier va piocher jusqu'à avoir 17 minimum.");
+	printf("--------------------------------------------------");
+	printf("Au bout de 9 manches, la personne qui a remporté le plus de manche a gagné");
+
+
+}
+
+/*------------------------------------------------------*/
+
+int main(int argc, char **argv) {
+  
+    int 	socket_descriptor, 	/* descripteur de socket */
+		longueur; 		/* longueur d'un buffer utilisé */
+    sockaddr_in adresse_locale; 	/* adresse de socket local */
+    hostent *	ptr_host; 		/* info sur une machine hote */
+    servent *	ptr_service; 		/* info sur service */
+    char 	buffer[256];
+    char *	prog; 			/* nom du programme */
+    char *	host; 			/* nom de la machine distante */
+    char *	mesg; 			/* message envoyé */
+    char    nbjoueur=1;		/*nombre de joueur dans la partie*/
+     
+    if (argc != 3) {
+	perror("usage : client <adresse-serveur> <numero port>");
+	exit(1);
+    }
+   prog = argv[0];
+    host = argv[1];
+    /*choix du num de port par le joueur*/
+    adresse_locale.sin_port = htons(argv[2]);
+    
+    printf("nom de l'executable : %s \n", prog);
+    printf("adresse du serveur  : %s \n", host);
+
+    if ((ptr_host = gethostbyname(host)) == NULL) {
+	perror("erreur : impossible de trouver le serveur a partir de son adresse.");
+	exit(1);
+    }
+    
+    /* copie caractere par caractere des infos de ptr_host vers adresse_locale */
+    bcopy((char*)ptr_host->h_addr, (char*)&adresse_locale.sin_addr, ptr_host->h_length);
+    adresse_locale.sin_family = AF_INET; /* ou ptr_host->h_addrtype; */
+
+    
+    printf("numero de port pour la connexion au serveur : %d \n", ntohs(adresse_locale.sin_port));
+    
+    /* creation de la socket */
+    if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	perror("erreur : impossible de creer la socket de connexion avec le serveur.");
+	exit(1);
+    }
+    
+    /* tentative de connexion au serveur dont les infos sont dans adresse_locale */
+    if ((connect(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
+	perror("erreur : impossible de se connecter au serveur.");
+	exit(1);
+    }
+    
+	/*--------------------------------------------------------------------------------------------*/
+    /*    CONNEXION ETABLIE     */
+
+    printf("Bienvenue dans le jeu du BlackJack !");
+
+	/* gestion des messages reçu du serveur */
+    while((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
+    	/*renseigner le nombre de joueurs*/
+		if (strcmp(buffer,"joueur")){
+    		printf("Donnez le nombre de joueur pour cette partie: \n");
+    		nbjoueur=getchar();
+    		sprintf(buffer,nbjoueur);
+    		/*envoie de la reponse au serveur*/
+    		renvoi(socket_descriptor,buffer);
+    	}
+    }
+    /*afficher les règles du Black Jack */
+	affichage_regle();
+
+    /* envoi du message vers le serveur */
+    if ((write(socket_descriptor, mesg, strlen(mesg))) < 0) {
+	perror("erreur : impossible d'ecrire le message destine au serveur.");
+	exit(1);
+    }
+    
+    /* mise en attente du prgramme pour simuler un delai de transmission */
+    sleep(3);
+     
+    printf("message envoye au serveur. \n");
+                
+    /* lecture de la reponse en provenance du serveur */
+    while((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
+	printf("reponse du serveur : \n");
+	write(1,buffer,longueur);
+    }
+    
+    printf("\nfin de la reception.\n");
+    
+    close(socket_descriptor);
+    
+    printf("connexion avec le serveur fermee, fin du programme.\n");
+    
+    exit(0);
+    
+}
