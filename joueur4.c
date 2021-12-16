f7dd4a0ffcddf31a95500706be5a398445567b50
Dartagnaa <73881529+Dartagnaa@users.noreply.github.com>
2021-12-16 00:19:37 -0800

    Version finale

--- joueur4.c
+++ joueur4.c
@@ -1,7 +1,7 @@
 
 /*-----------------------------------------------------------
 Client a lancer apres le serveur avec la commande :
-client <adresse-serveur> <message-a-transmettre>
+client <adresse-serveur> <port>
 ------------------------------------------------------------*/
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,7 +9,7 @@ client <adresse-serveur> <message-a-transmettre>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
-#include <unistd.h>			// For sleep and other functions 
+#include <unistd.h>			//pour sleep et d'autres fonctions 
 
 
 typedef struct sockaddr 	sockaddr;
@@ -17,7 +17,7 @@ typedef struct sockaddr_in 	sockaddr_in;
 typedef struct hostent 		hostent;
 typedef struct servent 		servent;
 
-/*-------------------------------------------------------*/
+//affichage des cartes dans le terminal
 void pic(int num)
 {
 	char fl;
@@ -52,7 +52,7 @@ void pic(int num)
 			printf("*******\n");
 			printf("*     *\n");
 			printf("* %c   *\n", fl);
-			printf("*   J *\n");
+			printf("*   V *\n");
 			printf("*     *\n");
 			printf("*******\n");
 			break;
@@ -62,7 +62,7 @@ void pic(int num)
 			printf("*******\n");
 			printf("*     *\n");
 			printf("* %c   *\n", fl);
-			printf("*   Q *\n");
+			printf("*   D *\n");
 			printf("*     *\n");
 			printf("*******\n");
 			break;
@@ -72,7 +72,7 @@ void pic(int num)
 			printf("*******\n");
 			printf("*     *\n");
 			printf("* %c   *\n", fl);
-			printf("*   K *\n");
+			printf("*   R *\n");
 			printf("*     *\n");
 			printf("*******\n");
 			break;
@@ -81,7 +81,7 @@ void pic(int num)
 	}
 }
 
-/*------------------------------------------------------*/
+
 void intialisationBuffer(char buffer[]){
 	for (int i = 0; i < 256; i++)
 	{
@@ -91,128 +91,128 @@ void intialisationBuffer(char buffer[]){
 	return;
 }
 
-/*------------------------------------------------------*/
+//envoi d'un message au serveur
 void envoi (int sock,char buffer[], char * message) {
 
-    /* modifie le buffer pour le renvoyer */
-    intialisationBuffer(buffer);    /*buffer vide*/
+    intialisationBuffer(buffer);
 
-    /* copier le message dans le buffer */
-    strncpy(buffer,message,256);
+    strncpy(buffer,message,256); //copier le message dans le buffer
 
-	/* mise en attente du prgramme pour simuler un delai de transmission */
-    sleep(3);
-    
-	/*ssize_t write(int fs, const void *buf, size_t N);*/
-    write(sock,buffer,strlen(buffer)+1);
+    sleep(3); //simulation délai
+
+    write(sock,buffer,strlen(buffer)+1); //écrire dans le buffer
 	return;
 }
 
-/*------------------------------------------------------*/
-void affichage_regle(){
-	printf("\nLe but est de faire 21 points.\n");
+
+void affichage_regle(int socket_descriptor, char *buffer){
+	char rep = ' '; //réponse du joueur
+
+	printf("\nLe but du jeu est de faire 21 points.\n");
 	printf("L'As vaut 1 ou 11, et les têtes valent 10\n");
-	printf("Si vous faites 21 points sue les 2 première cartes : Black Jack. Vous gagnez la manche\n");
-	printf("Sinon, vous pouvez piocher une carte. Mais attention, il ne faut pas dépasser 21\n");
-	printf("Si vous dépassez 21, vous perdez la manche, si vous faites moins, c'est au tour du croupier de tirer une carte\n");
-	printf("Le croupier va piocher jusqu'à avoir 17 minimum.\n");
+	printf("Si vous faites 21 points sur les 2 première cartes : Black Jack. Vous gagnez !\n");
+	printf("Sinon, vous pouvez piocher. Mais attention, il ne faut pas dépasser 21 points.\n");
+	printf("Si vous dépassez 21, vous perdez.\n");
+	printf("Si vous faites moins, c'est au tour du croupier de piocher.\n");
+	printf("Le croupier pioche jusqu'à avoir 17 minimum.\n");
+	printf("Si les deux parties obtiennent moins de 21, celui qui a le plus de point a gagné\n");
 	printf("--------------------------------------------------\n");
-	printf("La personne qui a remporté la partie a gagné\n");
+	printf("A vous de jouer ! Êtes-vous prêt à jouer? (o : oui)\n");
+	do{
+		rep = getchar();
+	}while (rep!='o');
+	envoi(socket_descriptor, buffer, &rep);
+	printf("--------------------------------------------------\n");
 	return;
 }
 
-/*------------------------------------------------------*/
+/*----------------------------------------*/
+/*                 MAIN                  */
+/*---------------------------------------*/
 
 int main(int argc, char **argv) {
   
-    int 	socket_descriptor, 	/* descripteur de socket */
-		longueur; 		/* longueur d'un buffer utilisé */
+    int socket_descriptor, 	// descripteur de socket
+		longueur; 		   // longueur d'un buffer utilisé
     sockaddr_in adresse_locale; 	/* adresse de socket local */
     hostent *	ptr_host; 		/* info sur une machine hote */
     servent *	ptr_service; 		/* info sur service */
     char 	buffer[256];
     char *	prog; 			/* nom du programme */
     char *	host; 			/* nom de la machine distante */
-	int  port;			/*num port*/
-    char *	mesg; 			/* message envoyé */
-    char    rep;		/*reponse du joueur*/
-     
+	int  port;			/*num port serveur*/
+
+	char d; //choix du joueur
+	int carte = 0;
+    
+	//vérifier que la syntaxe est respectée
     if (argc != 3) {
-	perror("usage : client <adresse-serveur> <numero port>");
-	exit(1);
+		perror("usage : client <adresse-serveur> <numero port>");
+		exit(1);
     }
    	prog = argv[0];
     host = argv[1];
-	/*choix du num de port par le joueur*/
-	port = atoi(argv[2]);
+
+	
+	port = atoi(argv[2]); /*choix du num de port par le joueur*/
 	adresse_locale.sin_port = htons(port);
 
-    printf("nom de l'executable : %s \n", prog);
-    printf("adresse du serveur  : %s \n", host);
-
     if ((ptr_host = gethostbyname(host)) == NULL) {
-	perror("erreur : impossible de trouver le serveur a partir de son adresse.");
-	exit(1);
+		perror("erreur : impossible de trouver le serveur a partir de son adresse.");
+		exit(1);
     }
     
     /* copie caractere par caractere des infos de ptr_host vers adresse_locale */
     bcopy((char*)ptr_host->h_addr, (char*)&adresse_locale.sin_addr, ptr_host->h_length);
-    adresse_locale.sin_family = AF_INET; /* ou ptr_host->h_addrtype; */
+    adresse_locale.sin_family = AF_INET;
 
-    
-    printf("numero de port pour la connexion au serveur : %d \n", ntohs(adresse_locale.sin_port));
-    
-    /* creation de la socket */
+    /* creation du socket */
     if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-	perror("erreur : impossible de creer la socket de connexion avec le serveur.");
-	exit(1);
+		perror("erreur : impossible de creer la socket de connexion avec le serveur.");
+		exit(1);
     }
     
     /* tentative de connexion au serveur dont les infos sont dans adresse_locale */
     if ((connect(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
-	perror("erreur : impossible de se connecter au serveur.");
-	exit(1);
+		perror("erreur : impossible de se connecter au serveur.");
+		exit(1);
     }
     
-/*--------------------------------------------------------------------------------------------*/
-    /*    CONNEXION ETABLIE     */
+
+   /*    CONNEXION ETABLIE     */
 
     printf("Bienvenue dans le jeu du BlackJack !\n");
 
-    /*afficher les règles du Black Jack */
-	affichage_regle();
+	affichage_regle(socket_descriptor,buffer); /*afficher les règles du Black Jack */
 
-/*--------------------------------------------------------------------------------------------*/
-	
 	/* ecoute en continue du serveur */
-	char d;
-	for(;;){
-		/* gestion des messages reçu du serveur */
-		if((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
-			//si le croupier envoi des cartes
-            //afficher carte du croupier
+	for(;;){ /* gestion des messages reçu du serveur */
+		
+		if((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) { //écoute d'un message
+			
+            //afficher carte du croupier si message = "croupier"
             if (strcmp(buffer,"croupier")==0){
 				printf("\nLa carte du croupier: \n");
                 sleep(2);
                 if((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
-                    int carte = 0;
-					if (memcmp(&buffer[0],"0",1)==0){
+					//vérif de la dizaine
+					if (memcmp(&buffer[0],"0",1)==0){ 
 						carte = buffer[1] - '0';
 					}else if(memcmp(&buffer[0],"1",1)==0){
 						carte = buffer[1] - '0';
 						carte = carte + 10;
 					}
-                    pic(carte);
+                    pic(carte); //afficher la carte dans terminal
 				}
             }
             
-            //afficher ses cartes
+            //afficher ses cartes si message = "joueur"
 			if (strcmp(buffer,"joueur")==0){
 				printf("Vos cartes : \n");
-                sleep(2);
 				intialisationBuffer(buffer);
+				sleep(2);
+
                 if((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
-                    int carte = 0;
 					//carte 1
 					if (memcmp(&buffer[0],"0",1)==0){
 						carte = buffer[1] - '0';
@@ -223,7 +223,6 @@ int main(int argc, char **argv) {
                     pic(carte);
 
 					//carte 2
-					carte = 0;
 					if (memcmp(&buffer[2],"0",1)==0){
 						carte = buffer[3] - '0';
 					}else if(memcmp(&buffer[2],"1",1)==0){
@@ -236,12 +235,13 @@ int main(int argc, char **argv) {
             
 			//choix de la valeur de l'As
 			if (strcmp(buffer,"valeur A")==0){
-				printf("Choisissez la valeur de l'As: 'o' pour 11 ou 'n' pour 1 :\n");
+				printf("Choisissez la valeur de l'As: ('o' : 11 || 'n' : 1) :\n");
 				do{
 					d = getchar();
 				} while (d!='o' && d!='n');
 				envoi(socket_descriptor, buffer, &d);
 			}
+
 			//afficher les points du joueur
 			if (strcmp(buffer,"points")==0){
 				printf("Voici vos points :");
@@ -258,28 +258,20 @@ int main(int argc, char **argv) {
 			}
 			
 
-			if (strcmp(buffer,"loose")==0){
-				printf("Vous avez perdu ! \n");
-				break;
-			}
-			if (strcmp(buffer,"win")==0){
-				printf("Vous avez gagné ! \n");
-				break;
-			}
-
-			// choisir une nouvelle carte
+			// choisir de piocher une carte
 			if (strcmp(buffer,"piocher?")==0){
-				printf("Voulez-vous piocher ? 'o' pour oui ou 'n' pour non :\n");
+				printf("Voulez-vous piocher ? ('o' : oui || 'n' : non) :\n");
 				do{
 					d = getchar();
 				} while (d!='o' && d!='n');
 				envoi(socket_descriptor, buffer, &d);
+
 				if(d=='o'){
 					printf("Votre nouvelle carte: \n");
-                	sleep(2);
 					intialisationBuffer(buffer);
+					sleep(2);
+
 					if((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
-						int carte = 0;
 						//nouvelle carte
 						if (memcmp(&buffer[0],"0",1)==0){
 							carte = buffer[1] - '0';
@@ -291,8 +283,22 @@ int main(int argc, char **argv) {
 					}
 				}
 				else if(d=='n'){
-					printf("Le croupier va piocher ses cartes voici d'abord sa première carte :\n");
+					printf("\nLe croupier va piocher. Voici d'abord sa première carte :\n");
 				}
+			}
+
+			//afficher les résultats
+			if (strcmp(buffer,"loose")==0){
+				printf("Vous avez perdu ! \n");
+				break;
+			}
+			if (strcmp(buffer,"egalite")==0){
+				printf("Vous êtes à égalité avec le croupier !\n");
+				break;
+			}
+			if (strcmp(buffer,"win")==0){
+				printf("Vous avez gagné ! \n");
+				break;
 			}
 		}
 	}
@@ -300,11 +306,5 @@ int main(int argc, char **argv) {
 	
 
 	/*Fermeture de la connexion*/
-    
-    
-    //printf("connexion avec le serveur fermee, fin du programme.\n");
-
-	
     close(socket_descriptor);
-    //exit(0);
 }
